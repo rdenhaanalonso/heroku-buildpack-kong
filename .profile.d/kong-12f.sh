c14b8219b45e5660ff11d466aa5bba816a5f0649
Mars Hall <mars@heroku.com>
2016-02-05 15:18:22 -0800

    Compat with common runtime & private spaces.

--- .profile.d/kong-12f.sh
+++ .profile.d/kong-12f.sh
@@ -9,12 +9,20 @@ set -u
 # Debug, echo every command
 #set -x
 
+echo "Starting Kong 12-factor config"
+
 SRC_DIR=$(pwd)
 BIN_DIR=$(cd "$(dirname "$0")"; pwd)
 
 # Get the private IP of the dyno.
 # Fallback to localhost for the common runtime.
-export KONG_CLUSTER_PRIVATE_IP="$(ip -4 -o addr show dev eth1 && (awk '{print $4}' | cut -d/ -f1) || echo '127.0.0.1')"
+export KONG_CLUSTER_PRIVATE_IP=$(ip -4 -o addr show dev eth1)
+if [ "$IP" ]
+then
+  KONG_CLUSTER_PRIVATE_IP=$(echo $IP | awk '{print $4}' | cut -d/ -f1)
+else
+  KONG_CLUSTER_PRIVATE_IP='127.0.0.1'
+fi
 echo "Kong cluster private IP: $KONG_CLUSTER_PRIVATE_IP"
 
 luajit $SRC_DIR/config/kong-12f.lua $SRC_DIR/config/kong.yml.etlua $SRC_DIR
